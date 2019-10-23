ac38ea8f86748d49700342bc1ea0e55a478d66f3
maany <imptodefeat@gmail.com>
2019-10-23 11:50:59 +0200

    rename slot to num_slots. Matches condor's NUM_SLOTS knob

--- sh/pre_config/files/config_50PC.py
+++ sh/pre_config/files/config_50PC.py
@@ -12,9 +12,13 @@ class WorkerConfig(ConfigFile):
         self.static_category.add_key_value("STARTER_ALLOW_RUNAS_OWNER", "false")
         self.static_category.add_key_value("EXECUTE_LOGIN_IS_DEDICATED", "true")
 
+    def add_lightweight_component_queried_parameters(self):
+        super().add_lightweight_component_queried_parameters()
+        self.lightweight_component_queried_category.add_key_value_query("num_slots", "$.config.num_slots")
+
     def add_advanced_parameters(self):
         super().add_advanced_parameters()
-        num_slots = self.lightweight_component['config']['slots']
+        num_slots = self.lightweight_component['config']['num_slots']
         for slot in range(0, num_slots):
             self.advanced_category.add_key_value("SLOT{slot}_USER".format(slot=slot), "slot{slot}".format(slot=slot))
         execution_id = self.lightweight_component['execution_id']
--- sh/pre_config/files/slots.py
+++ sh/pre_config/files/slots.py
@@ -7,6 +7,6 @@ class Slots(ConfigFile):
 
     def add_advanced_parameters(self):
         super().add_advanced_parameters()
-        num_slots = self.lightweight_component['config']['slots']
+        num_slots = self.lightweight_component['config']['num_slots']
         for slot in range(0, num_slots):
             self.advanced_category.add("slot{slot}\n".format(slot=slot))
